d7646ab666db89841b897dfa34565fc9a229a025
Kevin <grantcreative23@gmail.com>
2019-04-30 20:23:39 -0700

    Converting cards from static to dynamic

--- scripts.js
+++ scripts.js
@@ -1,8 +1,54 @@
+const game = document.getElementById('shootout')
 const cards = document.querySelectorAll('.memory-card')
 
 let hasFlippedCard = false
 let lockBoard = false
 let firstCard, secondCard
+
+// Game Modes
+let modes = [['easy', 16], ['medium', 20], ['hard', 30]]
+console.log({modes})
+
+
+class Card {
+  constructor(name, image, health, ammo) {
+    this.name = name
+    this.image = image
+    this.health = health
+    this.ammo = ammo
+  }
+}
+
+// Good Cards
+const ammoCards = [...Array(4)].map(i => new Card('ammunition', null, 1, null))
+const beerCards = [...Array(4)].map(i => new Card('beer', null, 1, null))
+const cigarCards = [...Array(2)].map(i => new Card('cigar', null, 1, null))
+const goodCards = [...ammoCards, ...beerCards, ...cigarCards]
+
+// Bad Cards
+const enemigoCards = [...Array(2)].map(i => new Card('enemigo', null, -1, null))
+const snakeCards = [...Array(2)].map(i => new Card('snake', null, -1, null))
+const scorpionCards = [...Array(2)].map(i => new Card('scorpion', null, -1, null))
+// const bandito = [...Array(2)].map(i => new Card('bandito', null, null, -1))
+const badCards = [...enemigoCards, ...snakeCards, ...scorpionCards]
+
+const allCards = [...goodCards, ...badCards]
+console.log({allCards})
+
+function dealCards() {
+  card = document.createElement('div')
+  allCards.forEach(function() {
+    clone = card.cloneNode()
+    clone.textContent = this.name
+    clone.className = 'memory-card'
+    clone.dataset.cardtype = 'cardtype'
+    
+    game.appendChild(clone)
+    console.log(clone)
+  })
+}
+dealCards()
+
 
 function flipCard() {
   if (lockBoard) return
